361e4f411c48e0dacb2bdf88df2a7f4f4d81dc74
naingyu <sktmdgus1212@naver.com>
2023-05-30 01:23:51 +0900

    init

---
+++ src/main/java/com/dbd/seoulcinema/SeoulcinemaApplication.java
@@ -0,0 +1,13 @@
+package com.dbd.seoulcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulcinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulcinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbd/seoulcinema/SeoulcinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbd.seoulcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulcinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
