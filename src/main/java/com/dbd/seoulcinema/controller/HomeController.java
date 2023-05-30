80870fb45f44c107311ce1220c05f882cb78a570
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-05-31 02:14:27 +0900

    feature: homepage rendering 완료

--- src/main/java/com/dbd/seoulcinema/controller/HomeController.java
+++ src/main/java/com/dbd/seoulcinema/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.dbd.seoulcinema.controller;
 import com.dbd.seoulcinema.domain.entity.Movie;
 import com.dbd.seoulcinema.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class HomeController {
 
     private final MovieRepository movieRepository;
@@ -27,20 +30,20 @@ public class HomeController {
         "movieImage": "http://~~~"
     }
  */
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(Model model){
         List<Movie> movieList = movieRepository.findAll();
 
         int len = movieList.size();
-        Map<String, Object> movieMap = new HashMap<>();
-        List<Movie> movie = new ArrayList<>();
+
+        List<Movie> movies = new ArrayList<>();
 
         //가장 마지막에 등록된 영화 (최신영화) 4개를 선별
-        for(int i=len-1; i<len-5; i--){
-            movie.add(movieList.get(i));
+        for(int i=len-1; i>len-5; i--){
+            movies.add(movieList.get(i));
         }
-        movieMap.put("movies", movie);
-        model.addAttribute("movies", movieMap);
+
+        model.addAttribute("movies", movies);
         return "home";
     }
 
