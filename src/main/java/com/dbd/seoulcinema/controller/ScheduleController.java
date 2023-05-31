5b2883fa53cc4f3b9a88cebf4259a282ddeb7af0
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-05-31 13:58:01 +0900

    feature: 상영일정조회 틀 생성

--- src/main/java/com/dbd/seoulcinema/controller/ScheduleController.java
+++ src/main/java/com/dbd/seoulcinema/controller/ScheduleController.java
@@ -1,16 +1,37 @@
 package com.dbd.seoulcinema.controller;
 
+import com.dbd.seoulcinema.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
     @GetMapping("/schedules")
-    public String getAllSchedules(@RequestParam Long movieNumber,
-                                  @RequestParam String screeningDate){
+    public String getAllSchedules(@RequestParam(required = false) Long movieNumber,
+                                  @RequestParam(required = false) String screeningDate){
+
+        if(movieNumber==null && screeningDate==null){
+            //예외처리
+        }else if(movieNumber == null && screeningDate != null){
+            // 상영하는 영화를 보여줘
+
+        }else if(movieNumber != null && screeningDate == null){
+            // 영화의 상영일정들을 보여줘
+
+        }else{ //둘다 널이 아닐 시에
+            //
+
+        }
 
         return "schedule";
     }
--- src/main/java/com/dbd/seoulcinema/service/ScheduleService.java
+++ src/main/java/com/dbd/seoulcinema/service/ScheduleService.java
@@ -1,7 +1,17 @@
 package com.dbd.seoulcinema.service;
 
+import com.dbd.seoulcinema.repository.MovieRepository;
+import com.dbd.seoulcinema.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Service
+@RequiredArgsConstructor
 public class ScheduleService {
+
+    private final MovieRepository movieRepository;
+    private final ScheduleRepository scheduleRepository;
+
 }
