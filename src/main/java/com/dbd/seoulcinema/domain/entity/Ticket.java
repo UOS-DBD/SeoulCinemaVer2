251668d6bc7cfe31eb97692016b713a35f36bbb3
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-06-02 09:39:53 +0900

    feat: 변수 타입 변경 및 엔티티 생성 메소드 추가

--- src/main/java/com/dbd/seoulcinema/domain/entity/Ticket.java
+++ src/main/java/com/dbd/seoulcinema/domain/entity/Ticket.java
@@ -4,6 +4,7 @@ import com.dbd.seoulcinema.domain.enumeration.TicketingStatus;
 
 import javax.persistence.*;
 
+import com.dbd.seoulcinema.vo.CreateTicketFinalVo;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,7 +41,17 @@ public class Ticket extends BaseTimeEntity {
     private String movieName;
 
     @OneToMany(mappedBy = "ticket")
-    private List<ScheduleSeat> scheduleSeats = new ArrayList<>();
+    private List<ScheduleSeat> scheduleSeats = new ArrayList<>(); //양방향 매핑
 
+    public static Ticket makeTicket(CreateTicketFinalVo vo, Member member, String movieName,List<ScheduleSeat> scheduleSeats) {
 
+        return Ticket.builder()
+                .ticketNumber(vo.getScheduleNumber() + vo.getSeats().get(0)) //티켓번호는 상영일정번호+좌석(첫 좌석= 인덱스 0 )번호
+                .ticketingStatus(TicketingStatus.Y)
+                .standardPrice(vo.getStandardPrice())
+                .member(member)
+                .scheduleSeats(scheduleSeats)
+                .movieName(movieName)
+                .build();
+    }
 }
