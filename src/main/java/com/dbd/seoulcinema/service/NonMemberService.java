74bfe9c8d8348d1b1db88044a4e70eaa44e925e7
naingyu <sktmdgus1212@naver.com>
2023-06-02 22:26:56 +0900

    nonmember session

--- src/main/java/com/dbd/seoulcinema/service/NonMemberService.java
+++ src/main/java/com/dbd/seoulcinema/service/NonMemberService.java
@@ -6,17 +6,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpSession;
+
 @Service
 @RequiredArgsConstructor
 public class NonMemberService {
     private final NonmemberRepository nonmemberRepository;
 
     @Transactional
-    public void createNonmember(String phoneNumber) {
+    public void createNonmember(String phoneNumber, HttpSession session) {
         System.out.println("service start");
         nonmemberRepository.save(NonMember.builder()
                 .phoneNumber(phoneNumber)
                 .build());
+        session.setAttribute("userId", phoneNumber);
         System.out.println("service end");
     }
 }
