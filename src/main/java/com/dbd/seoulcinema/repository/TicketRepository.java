fefbdcd3eca3b13b040a7236ed54c6a6e89adebc
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-06-03 10:44:29 +0900

    feat: 티켓 번호로 티켓 정보 조회 쿼리 추가

--- src/main/java/com/dbd/seoulcinema/repository/TicketRepository.java
+++ src/main/java/com/dbd/seoulcinema/repository/TicketRepository.java
@@ -1,8 +1,7 @@
 package com.dbd.seoulcinema.repository;
 
-import com.dbd.seoulcinema.domain.entity.Member;
-import com.dbd.seoulcinema.domain.entity.ScheduleSeat;
 import com.dbd.seoulcinema.domain.entity.Ticket;
+import com.dbd.seoulcinema.dao.ViewSpecificTicketDao;
 import com.dbd.seoulcinema.dto.ViewTicketsListDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -32,16 +31,15 @@ public interface TicketRepository extends JpaRepository<Ticket, String> {
             + "and t.nonMember.phoneNumber = :clientId")
     List<ViewTicketsListDto> findTicketListByNonMember(@Param("clientId") String clientId); //--> cross join 발생함! 추후 수정 예정
 
-
-    @Query("select ss "
-            + "from Ticket t, ScheduleSeat ss, Schedule s "
+    @Query("select distinct new com.dbd.seoulcinema.dao.ViewSpecificTicketDao(m.movieImage, m.movieName, s.screeningStartTime, " +
+            "s.screeningEndTime, s.screeningDate, th.theaterFloor) "
+            + "from Ticket t, ScheduleSeat ss, Schedule s, Movie m, Theater th "
             + "where t.ticketNumber = ss.ticket.ticketNumber "
             + "and ss.scheduleNumber = s.scheduleNumber "
-            + "and t.member.clientId = :memberId")
-    List<ScheduleSeat> findAllByMember(@Param("memberId") String memberId);
-
-   /* @Query("select t from Ticket t, Member m where t.member.clientId = m.clientId and m.clientId = :memberId")
-    Optional<Ticket> findTicketByMember(@Param("memberId") String memberId); */
+            + "and s.movie.movieNumber = m.movieNumber "
+            + "and s.theater.theaterNumber = th.theaterNumber "
+            + "and t.ticketNumber = :ticketNumber")
+    ViewSpecificTicketDao findSpecificTicketInfoById(@Param("ticketNumber") String ticketNumber);
 
     Optional<Ticket> findById(String number);
 
