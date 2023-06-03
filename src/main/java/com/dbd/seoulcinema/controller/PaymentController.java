ec3bb541c10166dc64b711f8c32d8e71011f0ecb
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-06-03 10:37:22 +0900

    feat: 회원 세션 전역 변수 추가

--- src/main/java/com/dbd/seoulcinema/controller/PaymentController.java
+++ src/main/java/com/dbd/seoulcinema/controller/PaymentController.java
@@ -19,16 +19,17 @@ import java.util.List;
 public class PaymentController {
 
 
+    //결제 타입을 고를 떄 호출하는 api
     @GetMapping("/payments/radio")
     public String viewPaymentRadio(HttpSession httpSession, Model model) {
 
-        String clientId = "mim501";
+
         String scheduleNumber = "20230601011";
-        List<Long> seats = new ArrayList<>(Arrays.asList(2L, 3L)); // 좌석 선택하는 api 완료된 후 @ModelAttribute 사용해서 코드 수정할 것
+        List<Long> seats = new ArrayList<>(Arrays.asList(9L, 10L)); // 좌석 선택하는 api 완료된 후 @ModelAttribute 사용해서 코드 수정할 것
         model.addAttribute(Constants.STANDARD_PRICE,12000*seats.size()); // 할인전 표준 금액 렌더링
 
         ScheduleSeatVo scheduleSeatVo = new ScheduleSeatVo(scheduleNumber, seats);
-        httpSession.setAttribute("clientId", "mim501");
+        httpSession.setAttribute(Constants.USER_ID_SESSION, "mim501");
         httpSession.setAttribute("scheduleSeatVo", scheduleSeatVo);
 
         return "paymentRadio";
