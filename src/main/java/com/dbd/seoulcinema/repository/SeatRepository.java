e3d821c316c24fdb644145497fefe26dc8e04b54
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-06-03 10:40:56 +0900

    feat: 티켓 번호로 좌석 조회 쿼리 추가

---
+++ src/main/java/com/dbd/seoulcinema/repository/SeatRepository.java
@@ -0,0 +1,18 @@
+package com.dbd.seoulcinema.repository;
+
+import com.dbd.seoulcinema.domain.entity.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+
+    @Query("select s from Seat s, ScheduleSeat ss, Ticket t "
+            + "where ss.ticket.ticketNumber = t.ticketNumber "
+            + "and ss.seatNumber.seatNumber = s.seatNumber "
+            + "and t.ticketNumber = :ticketNumber")
+    List<Seat> findAllSeatsByTicketNumber(@Param("ticketNumber") String ticketNumber);
+}
