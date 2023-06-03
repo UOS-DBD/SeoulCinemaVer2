079c6bb64da9b20952890cd1d2e1cb4b07b8913c
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-06-03 10:43:05 +0900

    feat: 티켓 목록, 상세 조회 controller 추가

--- src/main/java/com/dbd/seoulcinema/controller/TicketController.java
+++ src/main/java/com/dbd/seoulcinema/controller/TicketController.java
@@ -2,6 +2,8 @@ package com.dbd.seoulcinema.controller;
 
 import com.dbd.seoulcinema.domain.enumeration.DiscountType;
 import com.dbd.seoulcinema.domain.enumeration.PaymentType;
+import com.dbd.seoulcinema.dto.ViewSpecificTicketDto;
+import com.dbd.seoulcinema.dto.ViewTicketsListDto;
 import com.dbd.seoulcinema.vo.CreateTicketFinalVo;
 import com.dbd.seoulcinema.vo.CreateTicketVo;
 import com.dbd.seoulcinema.global.constants.Constants;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -27,24 +30,33 @@ public class TicketController {
     private final MemberService memberService;
 
 
- /*   @GetMapping("/tickets")
-    public String viewTicketList(@RequestParam("paymentType") String paymentType) {
+    @GetMapping("/tickets")
+    public String viewTicketList(HttpSession httpSession, Model model) {
 
-        //Object userId = httpSession.getAttribute("userId");
+        //String clientId = (String) httpSession.getAttribute(Constants.USER_ID_SESSION);
 
-        Optional<Ticket> findTickets = ticketService.viewTicketList();
+        List<ViewTicketsListDto> tickets = ticketService.viewTicketList("mim501"); // 추후 clientId로 변경해야함
+        model.addAttribute("tickets", tickets);
 
-        // model.addAttribute("tickets", findTickets);
-        System.out.println("!!!!!!!!!!!!" + paymentType);
-        return "index";
-    } */
+
+        return "viewTicketList";
+    }
+
+    @GetMapping("/tickets/{ticketNumber}")
+    public String viewSpecificTicket(@PathVariable("ticketNumber")String ticketNumber, Model model){
+
+        ViewSpecificTicketDto dto = ticketService.viewSpecificTicket(ticketNumber);
+        model.addAttribute("ticketInfo", dto);
+
+        return "viewSpecificTicket";
+    }
 
     @GetMapping("/tickets/radio")
     public String getTicketRadio(@RequestParam("paymentType") String paymentType, HttpSession httpSession, Model model) {
 
-        String userId = (String) httpSession.getAttribute("clientId");
-        Long memberPoint = memberService.getMemberPoint("mim501"); // mim501을 userId로 추후 변경해야함!
-        model.addAttribute(Constants.MEMBER_POINT, 10000);
+        String userId = (String) httpSession.getAttribute(Constants.USER_ID_SESSION);
+        Long memberPoint = memberService.getMemberPoint(userId);
+        model.addAttribute(Constants.MEMBER_POINT,memberPoint);
 
         if (paymentType.equals("CARD")) {
             httpSession.setAttribute(Constants.PAYMENT_TYPE, PaymentType.CARD);
@@ -59,22 +71,27 @@ public class TicketController {
 
     }
 
+    //최종 결제 버튼을 누를 때 호출하는 api >> 결제 완료하면 메인 화면으로 이동
     @PostMapping("/tickets")
     public String makeTickets(HttpSession httpSession) {
 
         CreateTicketFinalVo createTicketFinalVo = (CreateTicketFinalVo) httpSession.getAttribute("createTicketFinalVo");
-        System.out.println("scheduleNumber~!~!~!~!~!~!11111"+createTicketFinalVo.getScheduleNumber());
-        String memberId = (String) httpSession.getAttribute("clientId");
+
+        String clientId = (String) httpSession.getAttribute(Constants.USER_ID_SESSION);
         PaymentType paymentType = (PaymentType) httpSession.getAttribute(Constants.PAYMENT_TYPE);
-        System.out.println("scheduleNumber~!~!~!~!~!~!"+createTicketFinalVo.getScheduleNumber());
-        String result = ticketService.makeTicketsAndPayment(createTicketFinalVo, memberId,paymentType);
+
+        String result = ticketService.makeTicketsAndPayment(createTicketFinalVo, clientId,paymentType);
+
+        //TODO 결제 할인 테이블 생성하는 로직 추가해야 함
 
         return "home";
     }
 
+    //결제 정보를 입력한 후 최종 결제 금액을 보여줄때 호출하는 api
     @PostMapping("/tickets/final-check")
     public String makeTicketsFinalCheck(@ModelAttribute CreateTicketVo createTicketVo, HttpSession httpSession, Model model) {
 
+        //radio 박스에서 선택한 정보(결제 타입)가 어떤 정보인지 판단하는 로직
         Optional<DiscountType> findDiscountType = EnumSet.allOf(DiscountType.class)
                 .stream()
                 .filter(f -> f.toString().equals(createTicketVo.getDiscountType()))
@@ -82,8 +99,10 @@ public class TicketController {
 
         ScheduleSeatVo scheduleSeatVo = (ScheduleSeatVo) httpSession.getAttribute("scheduleSeatVo");
 
-        Integer totalPrice = ticketService.calculateTotalPrice(scheduleSeatVo.getSeats().size(), findDiscountType.get(), createTicketVo.getPoint());
+        Integer totalPrice = ticketService.calculateTotalPrice(scheduleSeatVo.getSeats().size(),
+                findDiscountType.get(), createTicketVo.getPoint());
 
+        //결제에 필요한 최종 정보를 저장하는 VO 생성
         CreateTicketFinalVo createTicketFinalVo = new CreateTicketFinalVo(createTicketVo,
                 scheduleSeatVo.getSeats().size(), totalPrice, findDiscountType.get(),scheduleSeatVo);
 
