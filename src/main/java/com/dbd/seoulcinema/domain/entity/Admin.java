7c3eae35e44b114ca198c0de1fa6494852ac28f2
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-06-04 15:29:01 +0900

    feature: 이미 예약된 좌석 선택 불가 기능 추가

--- src/main/java/com/dbd/seoulcinema/domain/entity/Admin.java
+++ src/main/java/com/dbd/seoulcinema/domain/entity/Admin.java
@@ -16,10 +16,8 @@ import lombok.*;
 public class Admin {
 
     @Id
-    @Column(name = "ADMIN_ID")
     private String adminId;
-
-    @Column(name = "PASSWORD")
+    
     private String password;
 
     //TODO(관리자등급)
