5b0af4f8c884a3415fe76166d0e37bcd64ebc3e8
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-06-04 10:53:08 +0900

    feature: 좌석 선택 기능 구현

---
+++ src/main/java/com/dbd/seoulcinema/controller/SeatController.java
@@ -0,0 +1,24 @@
+package com.dbd.seoulcinema.controller;
+
+import com.dbd.seoulcinema.service.SeatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class SeatController {
+    private final SeatService seatService;
+
+    @PostMapping("/api/schedule-seat")
+    public String processScheduleSeat(@RequestParam("selectedSeats") List<String> selectedSeats){
+        System.out.println(selectedSeats.size());
+        for (String seat : selectedSeats) {
+            System.out.println("Selected seat: " + seat);
+        }
+        return "schedules";
+    }
+}
