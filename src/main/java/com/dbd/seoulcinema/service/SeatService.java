eca4047771d552a9f724bbfe43a27c01bfbfcd88
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-06-05 15:41:22 +0900

    feature: 좌석 선택 후 결제하기 누를 시 결제 api에 scheduleSeat List 전달

--- src/main/java/com/dbd/seoulcinema/service/SeatService.java
+++ src/main/java/com/dbd/seoulcinema/service/SeatService.java
@@ -1,20 +1,42 @@
 package com.dbd.seoulcinema.service;
 
+import com.dbd.seoulcinema.domain.entity.Schedule;
+import com.dbd.seoulcinema.domain.entity.ScheduleSeat;
 import com.dbd.seoulcinema.domain.entity.Seat;
+import com.dbd.seoulcinema.domain.enumeration.PaymentStatus;
+import com.dbd.seoulcinema.repository.ScheduleRepository;
+import com.dbd.seoulcinema.repository.ScheduleSeatRepository;
 import com.dbd.seoulcinema.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SeatService {
     private final SeatRepository seatRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final ScheduleSeatRepository scheduleSeatRepository;
 
     public String makeSeatFormat(Seat seat){
         return seat.getRowNumber()+"-"+seat.getColNumber();
+    }
+
+    public ScheduleSeat saveSelectedSeat(String selectedSeat, String scheduleNumber){
+        String[] split = selectedSeat.split("-");
+        Seat seat = seatRepository.findByRowNumberAndColNumber(split[0], split[1]);
+        Schedule schedule = scheduleRepository.findById(scheduleNumber).get();
+
+        ScheduleSeat scheduleSeat = ScheduleSeat.builder()
+                .seatNumber(seat)
+                .paymentStatus(PaymentStatus.N)
+                .scheduleNumber(schedule)
+                .ticket(null)
+                .build();
+        return scheduleSeatRepository.save(scheduleSeat);
     }
 
 }
