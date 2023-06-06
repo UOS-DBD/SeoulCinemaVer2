80d12f0ea13506ca4b89d3bbc943edce0b051f74
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-06-06 14:08:07 +0900

    feat: 예매 취소 추가

--- src/main/java/com/dbd/seoulcinema/controller/TicketController.java
+++ src/main/java/com/dbd/seoulcinema/controller/TicketController.java
@@ -33,11 +33,9 @@ public class TicketController {
     @GetMapping("/tickets")
     public String viewTicketList(HttpSession httpSession, Model model) {
 
-        //String clientId = (String) httpSession.getAttribute(Constants.USER_ID_SESSION);
-
-        List<ViewTicketsListDto> tickets = ticketService.viewTicketList("mim501"); // 추후 clientId로 변경해야함
+        String clientId = (String) httpSession.getAttribute(Constants.USER_ID_SESSION);
+        List<ViewTicketsListDto> tickets = ticketService.viewTicketList(clientId); // 추후 clientId로 변경해야함
         model.addAttribute("tickets", tickets);
-
 
         return "viewTicketList";
     }
@@ -72,7 +70,7 @@ public class TicketController {
 
     }
 
-    //최종 결제 버튼을 누를 때 호출하는 api >> 결제 완료하면 메인 화면으로 이동
+    //최종 결제 버튼을 누를 때 호출하는 api >> 결제 완료하면 티켓 목록 조회로 이동
     @PostMapping("/tickets")
     public String makeTickets(HttpSession httpSession) {
 
@@ -81,35 +79,44 @@ public class TicketController {
         String clientId = (String) httpSession.getAttribute(Constants.USER_ID_SESSION);
         PaymentType paymentType = (PaymentType) httpSession.getAttribute(Constants.PAYMENT_TYPE);
 
-        String result = ticketService.makeTicketsAndPayment(createTicketFinalVo, clientId,paymentType);
+        String result = ticketService.makeTicketsAndPayment(createTicketFinalVo, clientId, paymentType);
 
-        //TODO 결제 할인 테이블 생성하는 로직 추가해야 함
-
-        return "home";
+        return "redirect:/api/tickets";
     }
 
     //결제 정보를 입력한 후 최종 결제 금액을 보여줄때 호출하는 api
     @PostMapping("/tickets/final-check")
     public String makeTicketsFinalCheck(@ModelAttribute CreateTicketVo createTicketVo, HttpSession httpSession, Model model) {
 
-        //radio 박스에서 선택한 정보(결제 타입)가 어떤 정보인지 판단하는 로직
+        //radio 박스에서 선택한 정보(할인 타입)가 어떤 정보인지 판단하는 로직
+        PaymentType paymentType = (PaymentType) httpSession.getAttribute(Constants.PAYMENT_TYPE);
         Optional<DiscountType> findDiscountType = EnumSet.allOf(DiscountType.class)
                 .stream()
                 .filter(f -> f.toString().equals(createTicketVo.getDiscountType()))
                 .findAny();
+        if (findDiscountType.isEmpty())
+            findDiscountType = Optional.of(DiscountType.ETC);
 
         ScheduleSeatVo scheduleSeatVo = (ScheduleSeatVo) httpSession.getAttribute("scheduleSeatVo");
-
-        Integer totalPrice = ticketService.calculateTotalPrice(scheduleSeatVo.getSeats().size(),
+        Integer totalPrice = ticketService.calculateTotalPriceCard(scheduleSeatVo.getSeats().size(),
                 findDiscountType.get(), createTicketVo.getPoint());
 
         //결제에 필요한 최종 정보를 저장하는 VO 생성
         CreateTicketFinalVo createTicketFinalVo = new CreateTicketFinalVo(createTicketVo,
-                scheduleSeatVo.getSeats().size(), totalPrice, findDiscountType.get(),scheduleSeatVo);
+                scheduleSeatVo.getSeats().size(), totalPrice, findDiscountType.get(), scheduleSeatVo);
 
         model.addAttribute("totalPrice", totalPrice);
         httpSession.setAttribute("createTicketFinalVo", createTicketFinalVo);
 
         return "paymentFinal";
+    }
+
+    //티켓 예매 취소할 때 사용하는 api
+    @DeleteMapping("/tickets")
+    public String cancelTicket(HttpSession httpSession) {
+
+        String ticketNumber = (String) httpSession.getAttribute("ticketNumber");
+        ticketService.cancelTicket(ticketNumber);
+        return "home";
     }
 }
