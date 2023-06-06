5e655913778a1014c9357453266175fc9e16f98c
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-06-06 14:50:47 +0900

    other: 통합

--- src/main/java/com/dbd/seoulcinema/repository/ScheduleSeatRepository.java
+++ src/main/java/com/dbd/seoulcinema/repository/ScheduleSeatRepository.java
@@ -4,9 +4,11 @@ import com.dbd.seoulcinema.domain.ScheduleSeatId;
 import com.dbd.seoulcinema.domain.entity.Schedule;
 import com.dbd.seoulcinema.domain.entity.ScheduleSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, ScheduleSeatId> {
@@ -17,6 +19,10 @@ public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, Sche
             + "and s.seatNumber.seatNumber in (:seats)")
     List<ScheduleSeat> findAllByScheduleNumberAndSeats(@Param("scheduleNumber") String scheduleNumber,
                                                        @Param("seats") List<Long> seats);
+
+    @Modifying
+    @Query("delete from ScheduleSeat ss where ss.paymentStatus = 1 and ss.createdDate >= :fiveMinutesAgo")
+    void deleteOldRecords(@Param("fiveMinutesAgo") LocalDateTime fiveMinutesAgo);
 
     @Query("select s from ScheduleSeat s "
             + "where s.ticket.ticketNumber = :ticketNumber")
