8b07ee9d6e3edac2b240e953e416fdb93b897ce9
naingyu <sktmdgus1212@naver.com>
2023-06-08 15:27:29 +0900

    .

--- src/main/java/com/dbd/seoulcinema/global/config/WebConfig.java
+++ src/main/java/com/dbd/seoulcinema/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.dbd.seoulcinema.global.config;
 
+import com.dbd.seoulcinema.global.interceptor.AdminLoginCheckInterceptor;
 import com.dbd.seoulcinema.global.interceptor.UserLoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,9 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserLoginCheckInterceptor())
                 .addPathPatterns("/**")
+                .order(1)
                 .excludePathPatterns("/home", "/", "/login", "/css/**", "*.ico", "/error",
                         "/admin/**", "/schedules", "/viewSchedulesForm", "/api/schedules",
-                        "/api/auth/login",
-                        "/api/**", "/assets/**");
+                        "/api/auth/login", "/api/admin/**");
+
+        registry.addInterceptor(new AdminLoginCheckInterceptor())
+                .addPathPatterns("/admin/**", "/api/admin/**")
+                .order(2)
+                .excludePathPatterns("/api/admin/auth/login", "/admin/login", "/admin/home");
     }
 }
